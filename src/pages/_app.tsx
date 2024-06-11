c3bc9bd9c82cf8fd16d9467872978a987572118c
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-11 13:23:03 +0100

    fixed lint error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.css";
-import { classNames } from "@/utils/helpers";
-import { Bricolage_Grotesque } from "next/font/google";
+// import { classNames } from "@/utils/helpers";
+// import { Bricolage_Grotesque } from "next/font/google";
 import type { AppProps } from "next/app";
 
-const bg = Bricolage_Grotesque({ subsets: ["latin"], display: "swap" });
+// const bg = Bricolage_Grotesque({ subsets: ["latin"], display: "swap" });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
