47da1a986c27301a61c497f5c45e138b458a050a
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-11 18:48:41 +0100

    added seo tags

--- src/components/HeadTemplate.tsx
+++ src/components/HeadTemplate.tsx
@@ -15,13 +15,47 @@ const HeadTemplate = ({ pic }: { pic: Item | undefined }) => {
 
   return (
     <Head>
-      <title>profile picture history - twitter @victoribironke_ </title>
+      <title>profile picture history - twitter @victoribironke_</title>
       <link rel="shortcut-icon" href={src} type="image/x-icon" />
       <link rel="icon" href={src} type="image/x-icon" />
       <meta
         name="description"
         content="history of the profile pictures of @victoribironke_ twitter account"
       />
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta
+        property="og:url"
+        content="https://profile-picture-automation.vercel.app/"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content="profile picture history - twitter @victoribironke_"
+      />
+      <meta
+        property="og:description"
+        content="history of the profile pictures of @victoribironke_ twitter account"
+      />
+      <meta property="og:image" content={src} />
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:domain"
+        content="www.profile-picture-automation.vercel.app/"
+      />
+      <meta
+        property="twitter:url"
+        content="https://profile-picture-automation.vercel.app/"
+      />
+      <meta
+        name="twitter:title"
+        content="profile picture history - twitter @victoribironke_"
+      />
+      <meta
+        name="twitter:description"
+        content="history of the profile pictures of @victoribironke_ twitter account"
+      />
+      <meta name="twitter:image" content={src} />
     </Head>
   );
 };
