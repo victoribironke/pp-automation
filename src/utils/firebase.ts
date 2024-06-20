61db0fa6627320c6d8265dd624b9bfdc682b5267
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-20 16:52:44 +0100

    added endpoint to get the latest image

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,14 @@
 import { auth, db } from "@/services/firebase";
+import { Item } from "@/types/general";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 
 export const firebaseLogin = async (email: string, password: string) => {
   const user = await signInWithEmailAndPassword(auth, email, password);
@@ -14,4 +22,12 @@ export const saveImageToFirebase = async (seed: string) => {
     timestamp: serverTimestamp(),
     type: "lorelei",
   });
+};
+
+export const getLatestImage = async () => {
+  const res = (await getDocs(collection(db, "twitter"))).docs.sort((a, b) =>
+    a.data().seed > b.data().seed ? 1 : -1
+  );
+
+  return res.at(-1)?.data() as Item;
 };
